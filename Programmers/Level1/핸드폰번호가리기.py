7324f76c46aaf435778590da76dea2bb59d46656
pySoo <55135881+pySoo@users.noreply.github.com>
2021-07-15 23:56:43 +0900

    Programmers_Level1

--- Programmers/Level1/핸드폰번호가리기.py
+++ Programmers/Level1/핸드폰번호가리기.py
@@ -1,7 +1,8 @@
 # 뒤 4자리 수를 제외한 번호를 가리는 문제
 # https://programmers.co.kr/learn/courses/30/lessons/12948
-# 파이썬의 리스트의 음수 인덱스는 뒤에서부터 문자열을 가져올 수 있게 합니다.
+# 파이썬의 리스트의 음수 인덱스를 앞에 배치하면, 뒤에서부터 문자열을 가져올 수 있게 합니다.
 def solution(phone_number):
     n = len(phone_number)
+    # 뒤에서 4번째부터 마지막 원소까지 저장
     back = phone_number[-4:]
     return "*" * (n - 4) + back
