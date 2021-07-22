b1a20fdb7dea268bbfe6a80acf05daa357eaa29e
pySoo <55135881+pySoo@users.noreply.github.com>
2021-07-22 18:11:29 +0900

    Programmers_Level2_BFS/DFS: 타겟 넘버

--- Programmers/Level2/타겟넘버.py
+++ Programmers/Level2/타겟넘버.py
@@ -2,6 +2,8 @@
 # https://programmers.co.kr/learn/courses/30/lessons/43165
 # nonlocal에 대해 배웠습니다. 전역 변수가 아닌 지역 변수를 다른 scope에서 사용하려면
 # nonlocal로 선언을 해야합니다. answer을 global로 설정한 경우 전역 변수가 아니기 때문에 not defined 에러가 발생합니다.
+
+# DFS 풀이
 def solution(numbers, target):
     answer = 0
     n = len(numbers)
@@ -16,3 +18,20 @@ def solution(numbers, target):
                 DFS(idx+1, result-numbers[idx])
     DFS(0,0)
     return answer
+
+# BFS 풀이
+# from collections import deque
+
+# def solution(numbers, target):
+#     answer = 0
+#     stack = deque([(0,0)])
+#     while stack:
+#         result, idx = stack.popleft()
+#         if idx == len(numbers):
+#             if result == target:
+#                 answer += 1
+#         else:
+#             stack.append((result+numbers[idx], idx+1))
+#             stack.append((result-numbers[idx], idx+1))
+    
+#     return answer
