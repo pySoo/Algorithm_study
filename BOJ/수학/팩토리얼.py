8b7a3c428cbfa98d4107a54b89ec273acd68e977
pySoo <55135881+pySoo@users.noreply.github.com>
2021-08-22 17:26:15 +0900

    BOJ_수학: 팩토리얼

---
+++ BOJ/수학/팩토리얼.py
@@ -0,0 +1,24 @@
+# https://www.acmicpc.net/problem/10872
+"""
+문제
+0보다 크거나 같은 정수 N이 주어진다. 이때, N!을 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 정수 N(0 ≤ N ≤ 12)가 주어진다.
+
+출력
+첫째 줄에 N!을 출력한다.
+"""
+import sys
+input = sys.stdin.readline
+
+
+def factorial(n):
+    result = 1
+    if n > 0:
+        result = n * factorial(n-1)
+    return result
+
+
+n = int(input())
+print(factorial(n))
