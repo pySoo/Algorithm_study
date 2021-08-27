0d6fa2b3767944e7ec0c5421b7cce2782531549e
pySoo <55135881+pySoo@users.noreply.github.com>
2021-08-27 17:32:16 +0900

    BOJ_분할정복: 배열 합치기

---
+++ BOJ/분할정복/배열합치기.py
@@ -0,0 +1,21 @@
+# https://www.acmicpc.net/problem/11728
+"""
+문제
+정렬되어있는 두 배열 A와 B가 주어진다. 두 배열을 합친 다음 정렬해서 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 배열 A의 크기 N, 배열 B의 크기 M이 주어진다. (1 ≤ N, M ≤ 1,000,000)
+둘째 줄에는 배열 A의 내용이, 셋째 줄에는 배열 B의 내용이 주어진다. 배열에 들어있는 수는 절댓값이 10^9보다 작거나 같은 정수이다.
+
+출력
+첫째 줄에 두 배열을 합친 후 정렬한 결과를 출력한다.
+"""
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+arr1 = list(map(int, input().split()))
+arr2 = list(map(int, input().split()))
+# extend를 써도 됨
+answer_list = arr1 + arr2
+print(' '.join(map(str, sorted(answer_list))))
