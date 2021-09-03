a6a4593acd4c0c349adfc6114fa5ca536cc9afa1
pySoo <55135881+pySoo@users.noreply.github.com>
2021-09-03 14:08:58 +0900

    BOJ_BFS/DFS: 숨바꼭질

--- BOJ/BFS_DFS/숨바꼭질.py
+++ BOJ/BFS_DFS/숨바꼭질.py
@@ -1,7 +1,7 @@
 # https://www.acmicpc.net/problem/1697
 """
 문제
-만약, 수빈이의 위치가 X일 때 걷는다면 1초 후에 X-1 또는 X+1로 이동하게 된다. 순간이동을 하는 경우에는 1초 후에 2*X의 위치로 이동하게 된다.
+수빈이는 동생과 숨바꼭질을 하고 있다. 수빈이는 현재 점 N(0 ≤ N ≤ 100,000)에 있고, 동생은 점 K(0 ≤ K ≤ 100,000)에 있다. 수빈이는 걷거나 순간이동을 할 수 있다. 만약, 수빈이의 위치가 X일 때 걷는다면 1초 후에 X-1 또는 X+1로 이동하게 된다. 순간이동을 하는 경우에는 1초 후에 2*X의 위치로 이동하게 된다.
 수빈이와 동생의 위치가 주어졌을 때, 수빈이가 동생을 찾을 수 있는 가장 빠른 시간이 몇 초 후인지 구하는 프로그램을 작성하시오.
 
 입력
@@ -10,26 +10,26 @@
 출력
 수빈이가 동생을 찾는 가장 빠른 시간을 출력한다.
 """
+import sys
 from collections import deque
-# k가 100,000 이하이므로
-MAX = 100001
+input = sys.stdin.readline
 n, k = map(int, input().split())
-time = [0] * MAX
+max = 10**5
+time = [0] * (max + 1)
 
 
 def bfs():
     q = deque()
     q.append(n)
     while q:
-        v = q.popleft()
-        if v == k:
-            print(time[v])
-            return
-        # 최단 거리를 구하기 위해 bfs를 사용하였고, 이동할 수 있는 경우의 수를 next_step의 인자로 설정하였습니다.
-        for next_step in (v-1, v+1, v*2):
-            if 0 <= next_step < MAX and not time[next_step]:
-                time[next_step] = time[v] + 1
-                q.append(next_step)
+        x = q.popleft()
+        if x == k:
+            print(time[x])
+            break
+        for nx in (x-1, x+1, x*2):
+            if 0 <= nx <= max and not time[nx]:
+                time[nx] = time[x] + 1
+                q.append(nx)
 
 
 bfs()
