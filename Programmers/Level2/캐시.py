83642ba50c27063e7e4ec4ef10ea2220c92f0fd8
pySoo <55135881+pySoo@users.noreply.github.com>
2021-09-07 17:33:21 +0900

    Programmers_Level2: 캐시

---
+++ Programmers/Level2/캐시.py
@@ -0,0 +1,41 @@
+# https://programmers.co.kr/learn/courses/30/lessons/17680
+"""
+문제
+지도개발팀에서 근무하는 제이지는 지도에서 도시 이름을 검색하면 해당 도시와 관련된 맛집 게시물들을 데이터베이스에서 읽어 보여주는 서비스를 개발하고 있다.
+이 프로그램의 테스팅 업무를 담당하고 있는 어피치는 서비스를 오픈하기 전 각 로직에 대한 성능 측정을 수행하였는데, 제이지가 작성한 부분 중 데이터베이스에서 게시물을 가져오는 부분의 실행시간이 너무 오래 걸린다는 것을 알게 되었다.
+어피치는 제이지에게 해당 로직을 개선하라고 닦달하기 시작하였고, 제이지는 DB 캐시를 적용하여 성능 개선을 시도하고 있지만 캐시 크기를 얼마로 해야 효율적인지 몰라 난감한 상황이다.
+
+어피치에게 시달리는 제이지를 도와, DB 캐시를 적용할 때 캐시 크기에 따른 실행시간 측정 프로그램을 작성하시오.
+
+제한사항
+캐시 크기(cacheSize)와 도시이름 배열(cities)을 입력받는다.
+cacheSize는 정수이며, 범위는 0 ≦ cacheSize ≦ 30 이다.
+cities는 도시 이름으로 이뤄진 문자열 배열로, 최대 도시 수는 100,000개이다.
+각 도시 이름은 공백, 숫자, 특수문자 등이 없는 영문자로 구성되며, 대소문자 구분을 하지 않는다. 도시 이름은 최대 20자로 이루어져 있다.
+
+입출력
+캐시크기(cacheSize)	도시이름(cities)	실행시간
+3	["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"]	50
+"""
+from collections import deque
+
+
+def solution(cacheSize, cities):
+    answer = 0
+    stack = deque()
+    if cacheSize == 0:
+        return len(cities) * 5
+    for city in cities:
+        city = city.lower()
+        # 히트일 때
+        if city in stack:
+            answer += 1
+            stack.remove(city)
+        else:
+            answer += 5
+
+        if len(stack) >= cacheSize:
+            stack.popleft()
+        stack.append(city)
+
+    return answer
