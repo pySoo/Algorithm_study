d1783806075070da61e0298c19f1363e85322d87
pySoo <55135881+pySoo@users.noreply.github.com>
2021-09-09 23:49:41 +0900

    Programmers_Level2: 파일명 정렬

---
+++ Programmers/Level2/파일명정렬.py
@@ -0,0 +1,47 @@
+# https://programmers.co.kr/learn/courses/30/lessons/17686
+"""
+문제
+무지는 단순한 문자 코드 순이 아닌, 파일명에 포함된 숫자를 반영한 정렬 기능을 저장소 관리 프로그램에 구현하기로 했다.
+
+소스 파일 저장소에 저장된 파일명은 100 글자 이내로, 영문 대소문자, 숫자, 공백(" "), 마침표("."), 빼기 부호("-")만으로 이루어져 있다. 파일명은 영문자로 시작하며, 숫자를 하나 이상 포함하고 있다.
+
+파일명은 크게 HEAD, NUMBER, TAIL의 세 부분으로 구성된다.
+
+HEAD는 숫자가 아닌 문자로 이루어져 있으며, 최소한 한 글자 이상이다.
+NUMBER는 한 글자에서 최대 다섯 글자 사이의 연속된 숫자로 이루어져 있으며, 앞쪽에 0이 올 수 있다. 0부터 99999 사이의 숫자로, 00000이나 0101 등도 가능하다.
+TAIL은 그 나머지 부분으로, 여기에는 숫자가 다시 나타날 수도 있으며, 아무 글자도 없을 수 있다.
+
+파일명을 세 부분으로 나눈 후, 다음 기준에 따라 파일명을 정렬한다.
+
+파일명은 우선 HEAD 부분을 기준으로 사전 순으로 정렬한다. 이때, 문자열 비교 시 대소문자 구분을 하지 않는다. MUZI와 muzi, MuZi는 정렬 시에 같은 순서로 취급된다.
+파일명의 HEAD 부분이 대소문자 차이 외에는 같을 경우, NUMBER의 숫자 순으로 정렬한다. 9 < 10 < 0011 < 012 < 13 < 014 순으로 정렬된다. 숫자 앞의 0은 무시되며, 012와 12는 정렬 시에 같은 같은 값으로 처리된다.
+두 파일의 HEAD 부분과, NUMBER의 숫자도 같을 경우, 원래 입력에 주어진 순서를 유지한다. MUZI01.zip과 muzi1.png가 입력으로 들어오면, 정렬 후에도 입력 시 주어진 두 파일의 순서가 바뀌어서는 안 된다.
+무지를 도와 파일명 정렬 프로그램을 구현하라.
+
+입출력
+입력: ["img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"]
+출력: ["img1.png", "IMG01.GIF", "img02.png", "img2.JPG", "img10.png", "img12.png"]
+
+입력: ["F-5 Freedom Fighter", "B-50 Superfortress", "A-10 Thunderbolt II", "F-14 Tomcat"]
+출력: ["A-10 Thunderbolt II", "B-50 Superfortress", "F-5 Freedom Fighter", "F-14 Tomcat"]
+"""
+
+
+def solution(files):
+    answer = []
+    for file in files:
+        head, number, tail = '', '', ''
+        number_check = False
+        for i in range(len(file)):
+            if file[i].isdigit():
+                number += file[i]
+                number_check = True
+            elif not number_check:
+                head += file[i]
+            else:
+                tail = file[i:]
+                break
+        answer.append((head, number, tail))
+
+    answer.sort(key=lambda x: (x[0].upper(), int(x[1])))
+    return [''.join(x) for x in answer]
