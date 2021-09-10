240844ad6d71d486e50cc8b503b71882334f917a
pySoo <55135881+pySoo@users.noreply.github.com>
2021-09-10 17:40:18 +0900

    Programmers_Level2: 방금 그 곡

---
+++ Programmers/Level2/방금그곡.py
@@ -0,0 +1,54 @@
+# https://programmers.co.kr/learn/courses/30/lessons/17683
+"""
+문제
+네오는 자신이 기억한 멜로디를 가지고 방금그곡을 이용해 음악을 찾는다. 그런데 라디오 방송에서는 한 음악을 반복해서 재생할 때도 있어서 네오가 기억하고 있는 멜로디는 음악 끝부분과 처음 부분이 이어서 재생된 멜로디일 수도 있다. 반대로, 한 음악을 중간에 끊을 경우 원본 음악에는 네오가 기억한 멜로디가 들어있다 해도 그 곡이 네오가 들은 곡이 아닐 수도 있다. 그렇기 때문에 네오는 기억한 멜로디를 재생 시간과 제공된 악보를 직접 보면서 비교하려고 한다. 다음과 같은 가정을 할 때 네오가 찾으려는 음악의 제목을 구하여라.
+
+방금그곡 서비스에서는 음악 제목, 재생이 시작되고 끝난 시각, 악보를 제공한다.
+네오가 기억한 멜로디와 악보에 사용되는 음은 C, C#, D, D#, E, F, F#, G, G#, A, A#, B 12개이다.
+각 음은 1분에 1개씩 재생된다. 음악은 반드시 처음부터 재생되며 음악 길이보다 재생된 시간이 길 때는 음악이 끊김 없이 처음부터 반복해서 재생된다. 음악 길이보다 재생된 시간이 짧을 때는 처음부터 재생 시간만큼만 재생된다.
+음악이 00:00를 넘겨서까지 재생되는 일은 없다.
+조건이 일치하는 음악이 여러 개일 때에는 라디오에서 재생된 시간이 제일 긴 음악 제목을 반환한다. 재생된 시간도 같을 경우 먼저 입력된 음악 제목을 반환한다.
+조건이 일치하는 음악이 없을 때에는 “(None)”을 반환한다.
+
+입출력
+m	musicinfos	answer
+"ABCDEFG"	["12:00,12:14,HELLO,CDEFGAB", "13:00,13:05,WORLD,ABCDEF"]	"HELLO"
+"CC#BCC#BCC#BCC#B"	["03:00,03:30,FOO,CC#B", "04:00,04:08,BAR,CC#BCC#BCC#B"]	"FOO"
+"ABC"	["12:00,12:14,HELLO,C#DEFGAB", "13:00,13:05,WORLD,ABCDEF"]	"WORLD"
+"""
+
+
+def get_minute(start, end):
+    hour = int(end.split(':')[0]) - int(start.split(':')[0])
+    minute = int(end.split(':')[1]) - int(start.split(':')[1])
+
+    if hour == 0:
+        return minute
+    else:
+        return hour * 60 + minute
+
+
+def change_melody(s):
+    s = s.replace('A#', 'a')
+    s = s.replace('C#', 'c')
+    s = s.replace('D#', 'd')
+    s = s.replace('F#', 'f')
+    s = s.replace('G#', 'g')
+    return s
+
+
+def solution(m, musicinfos):
+    answer = ('(None)', None)
+    m = change_melody(m)
+    max_time = 0
+
+    for info in musicinfos:
+        start, end, name, melody = info.split(',')
+        time = get_minute(start, end)
+        melody = change_melody(melody)
+        melody = (melody * time)[:time]
+        if m in melody:
+            if (answer[1] == None) or (time > answer[1]):
+                answer = (name, time)
+
+    return answer[0]
