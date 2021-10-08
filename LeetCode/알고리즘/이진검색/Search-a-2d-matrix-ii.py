be8cd3d89e8e81dab245746d37795b4a7f6fd354
pySoo <55135881+pySoo@users.noreply.github.com>
2021-10-08 23:43:38 +0900

    LeetCode_이진검색: Search a 2D matrix ii

---
+++ LeetCode/알고리즘/이진검색/Search-a-2d-matrix-ii.py
@@ -0,0 +1,43 @@
+# https://leetcode.com/problems/search-a-2d-matrix-ii/
+"""
+- Problem
+Write an efficient algorithm that searches for a target value in an m x n integer matrix. The matrix has the following properties:
+Integers in each row are sorted in ascending from left to right.
+Integers in each column are sorted in ascending from top to bottom.
+
+- Example 1:
+Input: matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 5
+Output: true
+
+- Example 2:
+Input: matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 20
+Output: false
+ 
+
+Constraints:
+
+m == matrix.length
+n == matrix[i].length
+1 <= n, m <= 300
+-10^9 <= matrix[i][j] <= 10^9
+All the integers in each row are sorted in ascending order.
+All the integers in each column are sorted in ascending order.
+-10^9 <= target <= 10^9
+"""
+
+
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        if not matrix:
+            return False
+        row = 0
+        col = len(matrix[0]) - 1
+
+        while row <= len(matrix) - 1 and col >= 0:
+            if target == matrix[row][col]:
+                return True
+            elif target < matrix[row][col]:
+                col -= 1
+            elif target > matrix[row][col]:
+                row += 1
+        return False
