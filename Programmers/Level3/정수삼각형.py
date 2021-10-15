58fa99f4be7cd6d26024447c0b7d288db20bfe4b
pySoo <55135881+pySoo@users.noreply.github.com>
2021-10-15 22:14:51 +0900

    Programmers_Level3: 정수 삼각형

--- Programmers/Level3/정수삼각형.py
+++ Programmers/Level3/정수삼각형.py
@@ -15,17 +15,12 @@ triangle	result
 
 
 def solution(triangle):
-    depth = len(triangle)
-    dp = []
-    for _ in range(depth-1):
-        dp.append([])
-    dp.append(triangle[depth-1])
-
-    for i in range(depth - 2, -1, -1):
-        rowLength = len(triangle[i])
-        for j in range(0, rowLength):
-            dp[i].append(max(dp[i+1][j] + triangle[i][j],
-                         dp[i+1][j+1] + triangle[i][j]))
-
-    answer = dp[0][0]
-    return answer
+    for i in range(1, len(triangle)):
+        for j in range(i+1):
+            if j == 0:
+                triangle[i][j] += triangle[i-1][j]
+            elif j == i:
+                triangle[i][j] += triangle[i-1][j-1]
+            else:
+                triangle[i][j] += max(triangle[i-1][j], triangle[i-1][j-1])
+    return max(triangle[-1])
